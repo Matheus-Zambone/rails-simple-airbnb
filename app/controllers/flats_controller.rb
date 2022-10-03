e648b8746e71ea6d369b9c8306d4c12cb8366eca
Matheus Zambone <matheus@zambonecomercial.com>
2022-10-03 14:34:14 -0300

    CRUD funcionando perfeitamente

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,4 +4,40 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :flats
+
+  # Read all flats
+  get 'flats', to: 'flats#index'
+
+  # Create a flat
+  get 'flats/new', to: 'flats#new', as: :new_flat
+  post 'flats', to: 'flats#create'
+
+  # Read one flat
+  get 'flats/:id', to: 'flats#show', as: :flat
+
+  # Update a flat
+  get 'flats/:id/edit', to: 'flats#edit', as: :edit_flat
+  patch 'flats/:id', to: 'flats#update'
+
+  # Delete a flat
+  delete 'flats/:id', to: 'flats#destroy'
 end
