8621d2bfafcff52a53385ca3b98903fa40569e38
Matheus Zambone <matheus@zambonecomercial.com>
2022-10-06 20:18:22 -0300

    Língua trocada para inglês

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,9 @@
 
 import { application } from "controllers/application"
 
+import ShowController from "./show_controller"
+application.register("show", ShowController)
+
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
---
+++ app/javascript/controllers/show_controller.js
@@ -0,0 +1,20 @@
+// import { Controller } from "@hotwired/stimulus";
+
+// export default class extends Controller{
+//     connect() {
+//     console.log("Show controller connected");
+//     this.displayAlertOnButtonClick();
+//     }
+
+//     displayAlertOnButtonClick() {
+//       // TODO: Select the big green button
+//       const button = this.element.querySelector("#btn-back");
+//       button.addEventListener("click", (event) => {
+//         alert(`Thank you!`);
+//       });
+//       // TODO: Bind the `click` event to the button
+//       // TODO: On click, display `Thank you!` in a JavaScript alert!
+//     };
+
+//     displayAlertOnButtonClick(); // Do not remove!
+// }
