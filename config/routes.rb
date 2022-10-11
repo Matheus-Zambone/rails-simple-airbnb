067de016be63046c787fa4fa289967bc359bf0a6
Matheus Zambone <matheus@zambonecomercial.com>
2022-10-11 15:43:24 -0300

    Mudanças na página edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,23 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # Read all flats
-  get 'flats', to: 'flats#index'
+  # # Read all flats
+  # get 'flats', to: 'flats#index'
 
-  # Create a flat
-  get 'flats/new', to: 'flats#new', as: :new_flat
-  post 'flats', to: 'flats#create'
+  # # Create a flat
+  # get 'flats/new', to: 'flats#new', as: :new_flat
+  # post 'flats', to: 'flats#create'
 
-  # Read one flat
-  get 'flats/:id', to: 'flats#show', as: :flat
+  # # Read one flat
+  # get 'flats/:id', to: 'flats#show', as: :flat
 
-  # Update a flat
-  get 'flats/:id/edit', to: 'flats#edit', as: :edit_flat
-  patch 'flats/:id', to: 'flats#update'
+  # # Update a flat
+  # get 'flats/:id/edit', to: 'flats#edit', as: :edit_flat
+  # patch 'flats/:id', to: 'flats#update'
 
-  # Delete a flat
-  delete 'flats/:id', to: 'flats#destroy'
+  # # Delete a flat
+  # delete 'flats/:id', to: 'flats#destroy'
+
+  root to: 'flats#index'
+  resources :flats
 end
